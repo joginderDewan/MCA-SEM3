7d0115555e277a813553a89fb10c8a382f42a47e
Debadyuti Karmakar <62183757+DEBADYUTI-KARMAKAR@users.noreply.github.com>
2021-10-01 19:33:39 +0530

    Span_Of_Array

---
+++ Span_Of_Array.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Span_Of_Array{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for(int i=0;i<a.length;i++){
+        a[i] = sc.nextInt();
+        
+    }
+    int min = a[0];
+    int max=a[0];
+    for(int i=1;i<a.length;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+        if(a[i]<min){
+            min = a[i];
+        }
+    }
+    int span = max - min;
+    System.out.println(span);
+    
+ }
+
+}
